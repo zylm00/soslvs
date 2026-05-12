ceae3e61c3c2e7e14cbd9556680b7bb327203a25
zylm00 <1123317122@qq.com>
2026-05-12 22:47:04 +0800

    Update main.dart

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -156,7 +156,7 @@ void runMainApp(bool startService) async {
   // Set window option.
   WindowOptions windowOptions = getHiddenTitleBarWindowOptions(
       isMainWindow: true, 
-      size: Size(270, 380),
+      size: Size(265, 375),
       alwaysOnTop: alwaysOnTop);
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     // Restore the location of the main window before window hide or show.
